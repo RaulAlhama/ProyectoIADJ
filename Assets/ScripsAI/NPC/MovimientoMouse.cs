2c92d9a77a9c1a41b6f3529a93c6f04f68af602d
RaulAlhama <raul.garciaa@um.es>
2023-03-04 19:21:06 +0100

    Seleccionar casi terminado

--- Assets/ScripsAI/NPC/MovimientoMouse.cs
+++ Assets/ScripsAI/NPC/MovimientoMouse.cs
@@ -4,27 +4,39 @@ using UnityEngine;
 
 public class MovimientoMouse : MonoBehaviour
 {
-    public List<GameObject> selectedNPCs;
-    private Vector3 targetPosition;
-    public Agent npcVirtual;
-    public GameObject puntero;
-    private bool selected;
+    public List<GameObject> selectedNPCs; //Lista de los agentes
+    private Vector3 targetPosition; // posición donde haremos click
+    public Agent npcVirtualPrefab; // NPC virtual pasado por parámetro
+    public GameObject punteroPrefab; // Puntero (en nuestro caso una esfera) pasado por parámetro
+
+    private Agent npcVirtual = null; // target virtual a instanciar
+    private GameObject puntero = null; // puntero a instanciar
+    private bool selected; //booleano que nos indica si se ha hecho clic en un lugar para ejecutar el movimiento de los agentes.
+    public GameObject indicadorPrefab = null;
 
     void Update()
     {
         // Comprueba si se ha hecho clic derecho en el mapa
         if (Input.GetMouseButtonDown(1) && selectedNPCs.Count > 0)
-        {
+        {   
+            if (puntero != null){ //Si existe puntero, lo eliminamos y también el targetVirtual
+                Destroy(puntero);
+                Destroy(npcVirtual.gameObject);
+            }
+
+           
             Ray ray = Camera.main.ScreenPointToRay(Input.mousePosition);
             RaycastHit hit;
             if (Physics.Raycast(ray, out hit))
             {
-            targetPosition = hit.point;
+            targetPosition = hit.point; //Nos quedamos con la posición donde hemos hecho click
             targetPosition.y = 0;
-            npcVirtual.transform.position = targetPosition;
-            puntero.transform.position = targetPosition;
-            Instantiate(npcVirtual);
-            Instantiate(puntero);
+            npcVirtualPrefab.transform.position = targetPosition; //Asignamos esa posición al target Virtual
+            punteroPrefab.transform.position = targetPosition; //Se la asignamos también al puntero
+
+            npcVirtual = Instantiate(npcVirtualPrefab); //creamos el target virtual
+            puntero = Instantiate(punteroPrefab);// creamos el puntero
+            
             selected = true;
 
             }
@@ -80,7 +92,7 @@ public class MovimientoMouse : MonoBehaviour
         {
             if(selected){
             AgentPlayer player = npcObject.GetComponent<AgentPlayer>();
-            player.target = npcVirtual;
+            player.target = npcVirtualPrefab; //Asignamos al target del agente, el target Virtual
             }
             
 
