d48104a874262cceccb09ed5ede8516ad58b2f9f
RaulAlhama <raul.garciaa@um.es>
2023-03-04 14:28:25 +0100

    Mejora movimiento Mouse

--- Assets/ScripsAI/NPC/MovimientoMouse.cs
+++ Assets/ScripsAI/NPC/MovimientoMouse.cs
@@ -4,29 +4,86 @@ using UnityEngine;
 
 public class MovimientoMouse : MonoBehaviour
 {
+    public List<GameObject> selectedNPCs;
+    private Vector3 targetPosition;
     public Agent npcVirtual;
-    public AgentPlayer player;
     public GameObject puntero;
+    private bool selected;
 
     void Update()
     {
-
-        if(player.selected && Input.GetMouseButtonDown(1)){
-            
+        // Comprueba si se ha hecho clic derecho en el mapa
+        if (Input.GetMouseButtonDown(1) && selectedNPCs.Count > 0)
+        {
+            Ray ray = Camera.main.ScreenPointToRay(Input.mousePosition);
             RaycastHit hit;
+            if (Physics.Raycast(ray, out hit))
+            {
+            targetPosition = hit.point;
+            targetPosition.y = 0;
+            npcVirtual.transform.position = targetPosition;
+            puntero.transform.position = targetPosition;
+            Instantiate(npcVirtual);
+            Instantiate(puntero);
+            selected = true;
 
-            if (Physics.Raycast(Camera.main.ScreenPointToRay(Input.mousePosition), out hit, 100)) {
-                    
-                    Vector3 aux = hit.point;
-                    aux.y = 0;
-                    npcVirtual.transform.position = aux;
-                    puntero.transform.position = aux;
-
-                    Instantiate(npcVirtual);
-                    Instantiate(puntero);
-                    player.target = npcVirtual;
-                    
             }
+        }
+
+        //Si pulsamos en un personaje mientras pulsamos control, se añadirá a la lista de personajes seleccionados
+        if (Input.GetMouseButtonDown(0) && Input.GetKey(KeyCode.LeftControl))
+        {
+            Ray ray = Camera.main.ScreenPointToRay(Input.mousePosition);
+            RaycastHit hit;
+            if (Physics.Raycast(ray, out hit))
+            {
+                GameObject npcObject = hit.collider.gameObject;
+                if (npcObject.CompareTag("NPC"))
+                {
+                    if (selectedNPCs.Contains(npcObject))
+                    {
+                        selectedNPCs.Remove(npcObject);
+                    }
+                    else
+                    {
+                        selectedNPCs.Add(npcObject);
+                    }
+                }
+                
+            }
+        }
+        //Si pulsamos en un personaje y no tenemos pulsado CONTROL la lista solo contendrá a este personaje
+        if (Input.GetMouseButtonDown(0) && !Input.GetKey(KeyCode.LeftControl))
+        {
+            Ray ray = Camera.main.ScreenPointToRay(Input.mousePosition);
+            RaycastHit hit;
+            if (Physics.Raycast(ray, out hit))
+            {
+                GameObject npcObject = hit.collider.gameObject;
+                if (npcObject.CompareTag("NPC"))
+                {
+                    if (selectedNPCs.Count > 0){
+                    selectedNPCs.Clear();
+                    }
+
+                    selectedNPCs.Add(npcObject);
+                } 
+                else{
+                    selectedNPCs.Clear();
+                }
+            }
+        }
+
+
+        // Mueve los NPC's seleccionados al destino
+        foreach (GameObject npcObject in selectedNPCs)
+        {
+            if(selected){
+            AgentPlayer player = npcObject.GetComponent<AgentPlayer>();
+            player.target = npcVirtual;
+            }
+            
+
         }
     }
 }
---
+++ Assets/ScripsAI/Steering/Formaciones/GridFinal.cs
@@ -0,0 +1,91 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using System;
+
+public class GridFinal 
+{
+    private int ancho;
+    private int largo;
+    private int[,] gridArray;
+    private float tam;
+    public const int PLAYER = 3;
+    public const int NPC = 2;
+    public const int LIBRE = 0;
+    public const int OBSTACULO = 1;
+
+    public GridFinal(int ancho, int largo, float tamCasilla){
+
+        this.ancho = ancho;
+        this.largo = largo;
+        this.tam = tamCasilla;
+
+        gridArray = new int[ancho,largo];
+        
+
+        for(int i = 0; i < gridArray.GetLength(0); i++){
+            for(int j = 0; j < gridArray.GetLength(1); j++){
+
+                Debug.DrawLine(getPosicionReal(i,j), getPosicionReal(i,j+1), Color.blue, 100f);
+                Debug.DrawLine(getPosicionReal(i,j), getPosicionReal(i+1,j), Color.blue, 100f);
+
+            }
+        }
+        Debug.DrawLine(getPosicionReal(0,largo), getPosicionReal(ancho,largo), Color.blue, 100f);
+        Debug.DrawLine(getPosicionReal(ancho,0), getPosicionReal(ancho,largo), Color.blue, 100f);
+
+        
+    }
+    public Vector3 getPosicionReal(int x, int y){
+
+        return new Vector3(x,0,y) * tam;
+    }
+    private void getCoordenadas(Vector3 posicionReal, out int i, out int j){
+
+        i = Mathf.FloorToInt(posicionReal.x / tam);
+        j = Mathf.FloorToInt(posicionReal.z / tam);
+    }
+    public void setValor(Vector3 pos, int value){
+
+        int x;
+        int y;
+        getCoordenadas(pos, out x,out y);
+        if((x < ancho && x >=0) && (y < largo  && y >=0))
+            gridArray[x,y] = value;
+    }
+    private void setValor(int i, int j, int value){
+
+        gridArray[i,j] = value;
+    }
+    public void setObstaculos(GameObject[] list){
+
+        foreach(GameObject obs in list){
+            //Debug.Log(obs.transform.tag);
+            setValor(obs.transform.position, OBSTACULO);
+        }
+        
+    }
+    public double[,] getGrafo(){ /// Vector3 posTarget
+
+        int iObjetivo = 15; //getCoordenadas
+        int jObjetivo = 10;
+        double[,] grafoMovimiento = new double[ancho,largo];
+
+        for(int i = 0; i < grafoMovimiento.GetLength(0); i++){
+            for(int j = 0; j < grafoMovimiento.GetLength(1); j++){
+                
+                if(gridArray[i,j] == OBSTACULO)
+                    grafoMovimiento[i,j] = Double.PositiveInfinity;
+                else
+                    grafoMovimiento[i,j] = Mathf.Abs(i-iObjetivo)+Mathf.Abs(j-jObjetivo);
+                
+                //if(grafoMovimiento[i,j] != Double.PositiveInfinity)
+                    //Debug.Log("(" + i + "," + j + ") : " +grafoMovimiento[i,j]);
+            }
+        }
+        return grafoMovimiento;
+    }
+    
+
+}
+
